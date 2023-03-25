86cdee275bf047b7fbc803238305f7e2a22045e3
caleb127 <matovucaleb@lbtvug.com>
2023-03-25 14:16:55 +0000

    push

---
+++ main.js
@@ -0,0 +1,18 @@
+var express=require('express')
+const app = express()
+const path = require("path")
+const helmet=require("helmet")
+app.use(helmet())
+app.use(helmet.hidePoweredBy())
+app.use("/public",express.static("public"))
+app.use(express.static(__dirname))
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"admin-page.html"))
+})
+app.get("/admin",(req,res)=>{
+    
+})
+app.use("/public",express.static("public"))
+
+app.listen('3000',
+    console.log("hi"))
