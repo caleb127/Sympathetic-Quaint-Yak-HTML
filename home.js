d098aab333131164669c1e82ef72bdaf9a41d281
caleb127 <matovucaleb@lbtvug.com>
2023-05-04 15:38:20 +0000

    hmmm......

--- home.js
+++ home.js
@@ -1,46 +1,29 @@
-gsap.registerPlugin(ScrollTrigger);
-const pageContainer = document.querySelector(".movies");
-/* SMOOTH SCROLL */
-const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true
-});
-scroller.on("scroll", ScrollTrigger.update);
-ScrollTrigger.scrollerProxy(pageContainer, {
-  scrollTop(value) {
-    return arguments.length
-      ? scroller.scrollTo(value, 0, 0)
-      : scroller.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      left: 0,
-      top: 0,
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  },
-  pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-window.addEventListener("load", function () {
-  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
-  let pinWrap = document.querySelector(".pin-wrap");
-  let pinWrapWidth = pinWrap.offsetWidth;
-  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-  // Pinning and horizontal scrolling
-  gsap.to(".pin-wrap", {
-    scrollTrigger: {
-      scroller: pageContainer, //locomotive-scroll
-      scrub: true,
-      trigger: "#sectionPin",
-      pin: true,
-      // anticipatePin: 1,
-      start: "top top",
-      end: pinWrapWidth
-    },
-    x: -horizontalScrollLength,
-    ease: "none"
-  });
-  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-  ScrollTrigger.refresh();
-});
+const categoriesWrapper = document.querySelector('.categories-wrapper')
+const images = document.querySelectorAll(".images img")
+categoriesWrapper.addEventListener('mousemove', e => {
+    gsap.to(images, {
+        // move images to mouse position
+        x: e.x, 
+        y: e.y, 
+        // transform origin of images to center
+        xPercent: -50, 
+        yPercent: -50,
+        // stagger subsequent images by 50ms
+        stagger: .05
+    })
+})
+gsap.utils.toArray('.categories h6')
+.forEach(category => {
+    let {label} = category.dataset
+    
+    category.addEventListener('mouseenter', () => {
+        gsap.to(`img[data-image=${label}]`, {opacity: 1, scale: 1})
+        gsap.set(`img[data-image=${label}]`, {zIndex: 1})
+        gsap.set(`h6[data-label=${label}]`, {zIndex: 2})
+    })
+    
+    category.addEventListener('mouseleave', () => {
+        gsap.to(`img[data-image=${label}]`, {opacity: 0, zIndex: -1, scale: .80})
+        gsap.set(`h6[data-label=${label}]`, {zIndex: 0})
+    })
+})
